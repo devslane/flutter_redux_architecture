b21ccf639ee17bbc2b4f70b1f4e6f20c37d636e7
Himanshu Yadav <himanshuyadav23196@gmail.com>
2019-04-11 01:12:40 +0530

    deserializing list of objects

--- lib/middleware/app_middleware.dart
+++ lib/middleware/app_middleware.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_demo/actions/actions.dart';
 import 'package:flutter_demo/models/app_state.dart';
+import 'package:flutter_demo/models/user.dart';
+import 'package:flutter_demo/serializers/types.dart';
 import 'package:redux/redux.dart';
+import 'package:flutter_demo/serializers/serializers.dart';
 
 Future<Null> appMiddleware(
     Store<AppState> store, action, NextDispatcher next) async {
--- lib/serializers/types.dart
+++ lib/serializers/types.dart
@@ -3,3 +3,5 @@ import 'package:built_value/serializer.dart';
 import 'package:flutter_demo/models/user.dart';
 
 const userList = FullType(BuiltList, [FullType(User)]);
+
+// To deserialize a list of user objects use serializers.deserialize(data, specifiedType: userList);
